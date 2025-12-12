247d71dcabdc3eebc59d0503859a34b40ef96901
Webrizen <webrizen@gmail.com>
2025-12-13 00:35:23 +0530

    minor css change

--- components/layout/site-header.tsx
+++ components/layout/site-header.tsx
@@ -7,7 +7,7 @@ import { Button } from "@/components/ui/button"
 
 export function SiteHeader() {
     return (
-        <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
             <div className="container flex h-14 max-w-screen-2xl items-center mx-auto px-4">
                 <div className="mr-4 flex">
                     <Link href="/" className="mr-6 flex items-center space-x-2">
